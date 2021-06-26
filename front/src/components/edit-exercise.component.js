ead3d1054442e0e524c6eb57a1837c68520ec6ed
Waqas Hussain <s113555@ap.be>
2021-06-26 17:48:19 +0200

    small bug fix

--- front/src/components/edit-exercise.component.js
+++ front/src/components/edit-exercise.component.js
@@ -86,7 +86,7 @@ export default class EditExercise extends Component {
 
     console.log(exercise);
 
-    axios.post('http://localhost:5000/exercise/update/' + this.props.match.params.id, exercise)
+    axios.put('http://localhost:5000/exercise/update/' + this.props.match.params.id, exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
